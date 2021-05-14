dfb5b34937f8f29a8428d6b201c5fb3d7e22eba1
Adam Natrop <adamnatrop@gmail.com>
2021-05-14 00:08:28 -0500

    trying to fix routes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Header />
         
         <Wrapper>
-          <Route exact path="/" component={About} />
+          <Route exact path="/react_portfolio/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} />
