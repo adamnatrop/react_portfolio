3c7f392f3687fa71691938b41aea5c5a666d71c8
Adam Natrop <adamnatrop@gmail.com>
2021-05-13 22:29:59 -0500

    added project database created mapping

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,13 +5,13 @@ function Project(props) {
 
   
     return (
-      <div className="project col-4 p-0 ">
-            <h2 className="m-0">Title</h2>
-            <img className="m-0" src={img} alt=""></img>
-            <p>Description</p>
-            <button type="button" className="btn btn-dark m-3">Live Site</button>
-            <button type="button" className="btn btn-dark">GitHub Repo</button>
-          </div>
+      <div className="project col-sm-4 p-0 ">
+        <h2 className="m-0">{props.title}</h2>
+        <img className="m-0" src={props.image} alt={props.title}></img>
+        <p>{props.description}</p>
+        <button type="button" className="btn btn-dark m-3"><a href={props.liveURL} target="_blank" rel="noreferrer">Live Site</a></button>
+        <button type="button" className="btn btn-dark"><a href={props.github} target="_blank" rel="noreferrer">GitHub Repo</a></button>
+      </div>
     );
   }
   
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,5 +1,6 @@
 import Project from "../../components/Project";
 import projectsData from "../../db/projects_db.json"
+import highlightProject from "../../db/highlight_project_db.json";
 
 function Projects(props) {
 
@@ -7,11 +8,19 @@ function Projects(props) {
     return (
       <div className="container">
         <div className="row justify-content-center mt-5 mb-5">
-          <Project className="col-sm-12" />
+          <Project className="col-sm-12" 
+            {...highlightProject}
+          />
           
         </div>
-        <div className="row justify-content-center">
-          <Project className="col-sm-12" />
+        <div className="row justify-content-center ">
+          {projectsData.map(project => (
+            <Project className=""
+              key={project.id} 
+              {...project}
+            />
+          ))}
+          
         </div>
         
       </div>
